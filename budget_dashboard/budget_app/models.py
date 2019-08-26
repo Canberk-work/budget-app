9199c278c9ebafb14be81aba00268839924eefe0
canberkh <canberkh@gmail.com>
2019-08-26 13:29:53 +0200

    Add models and random database populate script

--- budget_dashboard/budget_app/models.py
+++ budget_dashboard/budget_app/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Topic(models.Model):
-    top_name = models.CharField(max_length=264,unique=True)
+    top_name = models.CharField(max_length=264)
 
     def __str__(self):
         return self.top_name
 
 class Webpage(models.Model):
     topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
-    name = models.CharField(max_length=264,unique=True)
-    url = models.URLField(unique=True)
+    name = models.CharField(max_length=264)
+    url = models.URLField()
 
     def __str__(self):
         return self.name
